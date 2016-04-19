74d94cfb467933e02051315c06ccd50331a3bba4
Dan McFarland <dmcfarland930@gmail.com>
2016-04-19 19:42:25 -0400

    DMSW-2 - Committing PBD 9

--- PBD/VariablesAndNames/src/main/java/com/mycompany/variablesandnames/VariablesAndNames.java
+++ PBD/VariablesAndNames/src/main/java/com/mycompany/variablesandnames/VariablesAndNames.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ * /
 package com.mycompany.variablesandnames;
 
 /**
@@ -18,7 +18,7 @@ public class VariablesAndNames {
         //now some dubs
         double spaceInACar, carpoolCapacity, averagePassengersPerCar;
         
-        //give variables some value
+        //give variables some value.
         cars = 100;
         spaceInACar = 4;
         drivers = 30;
