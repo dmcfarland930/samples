0bda638453d96dc5ace83062286dba569da9f281
Dan McFarland <dmcfarland930@gmail.com>
2016-04-19 19:28:26 -0400

    add PBD folder

---
+++ PBD/MoreVariablesAndPrinting/src/main/java/com/mycompany/morevariablesandprinting/MoreVariablesAndPrinting.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.morevariablesandprinting;
+
+/**
+ *
+ * @author apprentice
+ */
+public class MoreVariablesAndPrinting {
+    
+    public static void main(String[] args){
+        
+        //Declare String and integer Variables
+        String myName, myEyes, myTeeth, myHair;
+        int myAge, myHeight, myWeight;
+        
+        //Give values to Strings and integers
+        myName = "Zed A. Shaw";
+        myAge = 35; //not a lie
+        myHeight = 74; //inches
+        myWeight = 180; //lbs
+        myEyes = "Blue";
+        myTeeth = "White";
+        myHair = "Brown";
+        
+        //Print out info about this Zed fella
+        System.out.println("Let's talk about "+ myName);
+        System.out.println("He's "+ myHeight + " inches tall.");
+        System.out.println("He's "+ myWeight + " pounds heavy.");
+        System.out.println("Actually that's not too heavy.");
+        System.out.println("He's got " + myEyes + " eyes and " + myHair + 
+                " hair.");
+        System.out.println("His teeth are usually " + myTeeth + " depending"
+                + "on the coffee.");
+        
+        //the tricky line
+        System.out.println("If I add " + myAge + ", " + myHeight + ", and " 
+                + myWeight + " I get " + (myAge + myHeight + myWeight) + ".");
+             
+    }
+    
+}
---
+++ PBD/YourSchedule/src/main/java/com/mycompany/yourschedule/YourSchedule.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.yourschedule;
+
+/**
+ *
+ * @author apprentice
+ */
+public class YourSchedule {
+    
+    public static void main (String[] args){
+        
+        //let's name some classes
+        String classOne, classTwo, classThree, classFour, classFive, classSix,
+                classSeven, classEight;
+        //name teachers
+        String teachOne, teachTwo, teachThree, teachFour, teachFive, teachSix,
+                teachSeven, teachEight;
+        
+        //assign values
+        classOne =  "Java 101";
+        classTwo =  "Git 101";
+        classThree =  "PoliSci 111";
+        classFour =  "Business 105";
+        classFive =  "English 101";
+        classSix =  "Physics 206";
+        classSeven =  "Philosophy 205";
+        classEight =  "Art 410";
+        teachOne = "Mr. Toner";
+        teachTwo = "Mr. Pudelski";
+        teachThree = "Mr. McCheese";
+        teachFour = "Mr. Trump";
+        teachFive = "Mr. Xiao";
+        teachSix = "Mr. Nye";
+        teachSeven = "Mr. Rogers";
+        teachEight = "Mr. Ross";
+        
+        System.out.println("+---------------------------------------+");
+        System.out.println("| 1 |            "+classOne+"|     "+teachOne+"|");
+        System.out.println("| 2 |             "+classTwo+"|  "+teachTwo+"|");
+        System.out.println("| 3 |         "+classThree+"|  "+teachThree+"|");
+        System.out.println("| 4 |        "+classFour+"|     "+teachFour+"|");
+        System.out.println("| 5 |         "+classFive+"|      "+teachFive+"|");
+        System.out.println("| 6 |         "+classSix+"|       "+teachSix+"|");
+        System.out.println("| 7 |      "+classSeven+"|    "+teachSeven+"|");
+        System.out.println("| 8 |             "+classEight+"|      "
+                +teachEight+"|");
+        System.out.println("+---------------------------------------+");        
+    }
+}
