8164494a638aa87b1574ad5f1184b0f1b1162e40
Dan McFarland <dmcfarland930@gmail.com>
2016-04-19 20:44:40 -0400

    DMSW-3 - Made changes to show metric values

--- PBD/MoreVariablesAndPrinting/src/main/java/com/mycompany/morevariablesandprinting/MoreVariablesAndPrinting.java
+++ PBD/MoreVariablesAndPrinting/src/main/java/com/mycompany/morevariablesandprinting/MoreVariablesAndPrinting.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor..
+ * and open the template in the editor.
  */
 package com.mycompany.morevariablesandprinting;
 
@@ -14,31 +14,35 @@ public class MoreVariablesAndPrinting {
     public static void main(String[] args){
         
         //Declare String and integer Variables
-        String myName, myEyes, myTeeth, myHair;
-        int myAge, myHeight, myWeight;
+        String name, eyes, teeth, hair;
+        int age;
+        float height, weight;
+        
         
         //Give values to Strings and integers
-        myName = "Zed A. Shaw";
-        myAge = 35; //not a lie
-        myHeight = 74; //inches
-        myWeight = 180; //lbs
-        myEyes = "Blue";
-        myTeeth = "White";
-        myHair = "Brown";
+        name = "Zed A. Shaw";
+        age = 35; //not a lie
+        height = Math.round(74 * 2.54f); //cm
+        weight = Math.round(180 / 2.2f); //kg
+        eyes = "Blue";
+        teeth = "White";
+        hair = "Brown";
         
         //Print out info about this Zed fella
-        System.out.println("Let's talk about "+ myName);
-        System.out.println("He's "+ myHeight + " inches tall.");
-        System.out.println("He's "+ myWeight + " pounds heavy.");
+        System.out.println("Let's talk about "+ name);
+        System.out.println("He's "+ height + " centimeters tall.");
+        System.out.println("He's "+ weight + " kilograms heavy.");
         System.out.println("Actually that's not too heavy.");
-        System.out.println("He's got " + myEyes + " eyes and " + myHair + 
+        System.out.println("He's got " + eyes + " eyes and " + hair + 
                 " hair.");
-        System.out.println("His teeth are usually " + myTeeth + " depending"
+        System.out.println("His teeth are usually " + teeth + " depending "
                 + "on the coffee.");
         
         //the tricky line
-        System.out.println("If I add " + myAge + ", " + myHeight + ", and " 
-                + myWeight + " I get " + (myAge + myHeight + myWeight) + ".");
+        System.out.println("If I add " + age + ", " + height + ", and " 
+                + weight + " I get " + (age + height + weight) + ".");
+        
+        
              
     }
     
