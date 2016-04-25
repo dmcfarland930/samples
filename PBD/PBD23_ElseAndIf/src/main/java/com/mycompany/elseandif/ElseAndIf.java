8c6e891262a1bd3cd7512fab17333050cde76abe
Dan McFarland <dmcfarland930@gmail.com>
2016-04-24 23:00:42 -0400

    DMSW - commit newly named PBD folders 9 - 28

---
+++ PBD/PBD23_ElseAndIf/src/main/java/com/mycompany/elseandif/ElseAndIf.java
@@ -0,0 +1,64 @@
+/*
+ * Else if creates another conditional statement to pass through to check 
+ * validity. Else is used when no statements are true.
+ * 
+ * Removing the else from else if creates a brand new statement. Because the 
+ * statement cars < people is not true, it turns to else.
+ */
+package com.mycompany.elseandif;
+
+/**
+ *
+ * @author apprentice
+ */
+public class ElseAndIf {
+    
+    public static void main(String[] args) {
+        
+        int people = 30;
+        int cars = 40;
+        int buses = 15;
+        
+        //compare cars and people
+        if (cars > people){
+            
+            System.out.println("We should take the cars.");
+        
+        } else if (cars < people) {
+            
+            System.out.println("We should not take the cars.");
+            
+        } else {
+            
+            System.out.println("We can't decide.");
+            
+        }
+        
+        //compare buses to cars
+        if (buses > cars){
+            
+            System.out.println("That's too many buses.");
+        
+        } else if (buses < cars) {
+            
+            System.out.println("Maybe we could take the buses.");
+            
+        } else {
+            
+            System.out.println("We still can't decide.");
+            
+        }
+        
+        //compare buses to people
+        if (people > buses){
+            
+            System.out.println("All right, let's just take the buses.");
+            
+        } else {
+            
+            System.out.println("Fine let's stay home then.");
+            
+        }
+    }
+    
+}
---
+++ PBD/PBD26_SpaceBoxing/src/main/java/com/mycompany/spaceboxing/SpaceBoxing.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.spaceboxing;
+
+import java.util.Scanner;
+import java.text.*;
+
+/**
+ *
+ * @author apprentice
+ */
+public class SpaceBoxing {
+    
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#.##");
+        
+        int planet;
+        double earthWt;
+        double weight = 0;        
+        boolean valid = false;
+        
+        System.out.print("Please enter your earth weight: ");
+            earthWt = keyboard.nextDouble();
+        
+        //run loop until user enters a valid planet number    
+        while (!valid){    
+        System.out.println("");
+        System.out.println("I have information on the following planets:");
+        
+        System.out.println("1. Venus   2. Mars    3. Jupiter");
+        System.out.println("4. Saturn  5. Uranus  6. Neptune");
+            
+        System.out.println("");
+        System.out.print("Which planet are you visiting? ");
+            planet = keyboard.nextInt();
+            
+            
+            switch(planet){
+                
+                case 1:
+                    weight = earthWt * 0.78;
+                    valid = true;
+                    break;
+                    
+                case 2:
+                    weight = earthWt * 0.39;
+                    valid = true;
+                    break;
+                
+                case 3:
+                    weight = earthWt * 2.65;
+                    valid = true;
+                    break;
+                
+                case 4:
+                    weight = earthWt * 1.17;
+                    valid = true;
+                    break;
+                
+                case 5:
+                    weight = earthWt * 1.05;
+                    valid = true;
+                    break;    
+                
+                case 6:
+                    weight = earthWt * 1.23;
+                    valid = true;
+                    break;
+                    
+                default:
+                    System.out.println("");
+                    System.out.println("That planet does not exist. ");
+                    System.out.println("Please enter a valid planet "
+                            + "number. ");
+                    
+            }
+        }    
+            
+        System.out.println("");
+        System.out.println("Your weight would be "+df.format(weight)+ 
+                " pounds on that planet.");
+    }
+    
+}
