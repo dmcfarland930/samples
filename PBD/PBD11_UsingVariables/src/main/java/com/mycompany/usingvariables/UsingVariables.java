78c62414f9701715d235a8c53242901f65f1396e
Dan McFarland <dmcfarland930@gmail.com>
2016-04-24 23:03:46 -0400

    DMSW-26 Added PBD 09-28 with new names

---
+++ PBD/PBD11_UsingVariables/src/main/java/com/mycompany/usingvariables/UsingVariables.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.usingvariables;
+
+/**
+ *
+ * @author apprentice
+ */
+public class UsingVariables {
+    
+    public static void main (String[] args){
+        
+        //Declare our variables
+        int roomNumber = 113;
+        double weirdNumber = 2.71828;
+        String className = "Computer Science";
+        
+        //Print out fun lines
+        System.out.println("This is room #"+roomNumber);
+        System.out.println("e is close to "+weirdNumber);
+        System.out.println("I am learning a bit about "+className);
+        
+    }
+    
+}
---
+++ PBD/PBD15_TheForgetfulMachine/src/main/java/com/mycompany/theforgetfulmachine/TheForgetfulMachine.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.theforgetfulmachine;
+
+import java.util.Scanner;
+/**
+ *
+ * @author apprentice
+ */
+public class TheForgetfulMachine{
+    
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.print("Give me a word! ");
+        keyboard.nextLine();
+        System.out.print("Give me a second word! ");
+        keyboard.nextLine();
+        System.out.println("");
+        
+        System.out.print("Great, now your favorite number? ");
+        keyboard.nextLine();
+        System.out.print("And your second-favorite number... ");
+        keyboard.nextLine();
+        System.out.println("Whew! Wasn't that fun?");
+        
+    }
+}
