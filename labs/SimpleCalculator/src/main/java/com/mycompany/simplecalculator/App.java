d5cdc69df7d60c5314e8a305430e1345d17d4875
Dan McFarland <dmcfarland930@gmail.com>
2016-05-01 22:22:43 -0400

    DMSW-31 commit my simple calculator and flowchart

---
+++ labs/SimpleCalculator/src/main/java/com/mycompany/simplecalculator/App.java
@@ -0,0 +1,19 @@
+/*
+ * This program was written by Daniel McFarland.
+ * I hope you enjoy it!
+ */
+package com.mycompany.simplecalculator;
+
+/**
+ *
+ * @author apprentice
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        CalculatorInterface ci = new CalculatorInterface();
+        ci.run();
+
+    }
+}
